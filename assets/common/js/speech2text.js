e529dc27aa53d33b7472564423bdb1ac51b89c42
Ikemefuna <nwankwoikemefuna23@gmail.com>
2021-06-03 13:26:25 +0100

    Improved punctuation

--- assets/common/js/speech2text.js
+++ assets/common/js/speech2text.js
@@ -54,7 +54,11 @@ $(document).ready(function(){
             let isFinal = result.isFinal && (result[0].confidence > 0);
             if (isFinal) {
                 transcript = result[0].transcript;
-                final_transcript += capitalize2(result[0].transcript + '.') + (isMobile() ? ' ' : '');
+                if (isMobile()) {
+                    final_transcript += result[0].transcript;
+                } else {
+                    final_transcript += capitalize2(result[0].transcript + '.');
+                }
                 if (transcript == last_debounce_transcript) {
                     return;
                 }
